5a5c4e898d6401fb5a1845e7194009dd0fe92bd2
m2nsp <mini071663@gmail.com>
2024-10-08 00:59:55 +0900

    엔티티 개발 - 연관관계 있음

--- src/main/kotlin/com/m2nsp/portfolio/domain/entity/Experience.kt
+++ src/main/kotlin/com/m2nsp/portfolio/domain/entity/Experience.kt
@@ -3,10 +3,75 @@ package com.m2nsp.portfolio.domain.entity
 import jakarta.persistence.*
 
 @Entity
-class Experience : BaseEntity() {
+class Experience(
+    title: String,
+    description: String,
+    startYear: Int,
+    startMonth: Int,
+    endYear: Int?,      //비어있는 경우 : 진행 중인 프로젝트
+    endMonth: Int?,
+    isActive: Boolean
+) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "experience_id")
     var id: Long? = null
+
+    var title: String = title
+
+    var description: String = description
+
+    var startYear: Int = startYear
+
+    var startMonth: Int = startMonth
+
+    var endYear: Int? = endYear
+
+    var endMonth: Int? = endMonth
+
+    var isActive: Boolean = isActive
+
+    @OneToMany(
+        targetEntity = ExperienceDetail::class,
+        fetch = FetchType.LAZY,
+        cascade = [CascadeType.ALL]
+    )
+    @JoinColumn(name = "experience_id")     //맵핑의 기준이 되는 칼럼을 알려줌
+    var details: MutableList<ExperienceDetail> = mutableListOf()
+    //Experience : ExperienceDetail = 1:n
+
+    fun getEndYearMonth(): String {
+        if (endYear == null || endMonth == null) {
+            return "Present"
+        }
+
+        return "${endYear}.${endMonth}"
+    }
+
+    fun update(
+        title: String,
+        description: String,
+        startYear: Int,
+        startMonth: Int,
+        endYear: Int?,
+        endMonth: Int?,
+        isActive: Boolean
+    ) {
+        //받아온 데이터들을 필드에 업데이트 해주는 기능
+        this.title = title
+        this.description = description
+        this.startYear = startYear
+        this.startMonth = startMonth
+        this.endYear = endYear
+        this.endMonth = endMonth
+        this.isActive = isActive
+    }
+
+    fun addDetails(details: MutableList<ExperienceDetail>?) {
+        if (details != null) {
+            this.details.addAll(details)
+        }
+    }
+
 }
