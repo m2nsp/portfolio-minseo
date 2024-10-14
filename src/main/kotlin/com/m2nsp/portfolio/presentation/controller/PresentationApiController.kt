e600cee91cf2a11696cacca585fec81635468053
m2nsp <mini071663@gmail.com>
2024-10-15 02:32:23 +0900

    컨트롤러 개발

--- src/main/kotlin/com/m2nsp/portfolio/presentation/controller/PresentationApiController.kt
+++ src/main/kotlin/com/m2nsp/portfolio/presentation/controller/PresentationApiController.kt
@@ -1,15 +1,41 @@
 package com.m2nsp.portfolio.presentation.controller
 
+import com.m2nsp.portfolio.presentation.dto.IntroductionDTO
+import com.m2nsp.portfolio.presentation.dto.LinkDTO
+import com.m2nsp.portfolio.presentation.dto.ProjectDTO
+import com.m2nsp.portfolio.presentation.dto.ResumeDTO
+import com.m2nsp.portfolio.presentation.service.PresentationService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+//데이터의 전달에 집중
+
 @RestController
 @RequestMapping("/api")
-class PresentationApiController {
+class PresentationApiController(
+    private val presentationService: PresentationService
+) {
 
     @GetMapping("/test")
     fun test(): String {
         return "OK"
+    }
+
+    @GetMapping("/v1/introductions")
+    fun getIntroductions(): List<IntroductionDTO>{
+        return presentationService.getIntroductions()
+    }
+    @GetMapping("/v1/links")
+    fun getLinks(): List<LinkDTO>{
+        return presentationService.getLinks()
+    }
+    @GetMapping("/v1/resume")
+    fun getResume(): ResumeDTO{
+        return presentationService.getResume()
+    }
+    @GetMapping("/v1/projects")
+    fun getProjects(): List<ProjectDTO>{
+        return presentationService.getProjects()
     }
 }
