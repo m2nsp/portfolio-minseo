992096d765f656d75d06924c5be4f6556389eec0
m2nsp <mini071663@gmail.com>
2024-10-14 13:06:00 +0900

    리포지토리 성능 개선

--- src/main/kotlin/com/m2nsp/portfolio/domain/repository/ExperienceRepository.kt
+++ src/main/kotlin/com/m2nsp/portfolio/domain/repository/ExperienceRepository.kt
@@ -2,12 +2,16 @@ package com.m2nsp.portfolio.domain.repository
 
 import com.m2nsp.portfolio.domain.entity.Experience
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import java.util.*
 
 interface ExperienceRepository : JpaRepository<Experience, Long> {
 
+    @Query("select e from Experience e left join fetch e.details where e.isActive = :isActive")
     fun findAllByIsActive(isActive: Boolean): List<Experience>
 
+    //이 경우 원래 교집합(inner join) -- 그러면 양쪽에 모두 데이터가 있어야 가져올 수 있음 --여기서는 details가 크게 의미있지 않아서 left join 사용
+    @Query("select e from Experience e left join fetch e.details where e.id = :id")
     override fun findById(id: Long): Optional<Experience>
     //JpaRepository에 정의되어있기 때문에 오버라이딩 해줘야됨
 }
--- src/test/kotlin/com/m2nsp/portfolio/domain/repository/ExperienceRepositoryTest.kt
+++ src/test/kotlin/com/m2nsp/portfolio/domain/repository/ExperienceRepositoryTest.kt
@@ -85,4 +85,11 @@ class ExperienceRepositoryTest(
         }
         println("----- findAllByIsActive 테스트 종료 -----")
     }
+/*
+    @Test
+    fun test(){
+        val findById = experienceRepository.findById(1)
+        println(findById.isPresent)
+    }
+ */
 }
